42b5ef86487275a7dcbafb89c283558d19ac3fdb
Dave Ceddia <dave@daveceddia.com>
2020-08-06 19:15:10 -0400

    add a benchmark script

---
+++ packages/shiki/src/__tests__/benchmark.js
@@ -0,0 +1,26 @@
+const { getHighlighter } = require('shiki')
+const fs = require('fs')
+
+const t = () => Date.now()
+const printTime = (desc, t0, t1) => console.log((t1 - t0).toString().padStart(5, ' '), 'ms', desc)
+
+;(async function run() {
+  console.log('Starting benchmark...')
+
+  const data = fs.readFileSync('react-corpus-3519-lines.js', 'utf8')
+  const t0 = t()
+
+  const highlighter = await getHighlighter({
+    theme: 'nord'
+  })
+
+  const t1 = t()
+
+  const out = highlighter.codeToHtml(data, 'js')
+
+  const t2 = t()
+
+  printTime('getHighlighter', t0, t1)
+  printTime('codeToHtml', t1, t2)
+  printTime('total', t0, t2)
+})()
